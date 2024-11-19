f69bc7774d80361b1cd2f154d5944c88b66c205f
test <ccccssung@naver.com>
2024-11-19 18:55:37 +0900

    commit

---
+++ src/main/java/com/kong/cc/dto/ItemMiddleCategoryForm.java
@@ -0,0 +1,18 @@
+package com.kong.cc.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemMiddleCategoryForm {
+
+    private String itemCategoryName;
+    private Integer itemCategoryMajorNum;
+
+}
