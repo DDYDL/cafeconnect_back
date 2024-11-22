d68604337c6fd2d880968aba483ff24865ec6f10
Dayo <dainning00@gmail.com>
2024-11-22 15:14:35 +0900

    add main

--- src/main/java/com/kong/cc/controller/Maincontroller.java
+++ src/main/java/com/kong/cc/controller/Maincontroller.java
@@ -1,11 +1,28 @@
 package com.kong.cc.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.kong.cc.dto.StoreDto;
 
 @RestController
 public class Maincontroller {
 	
 //	@GetMapping("/selectMenu") // IntroMain.js
+//	public ResponseEntity<List<StoreDto>> selectStoreList(@PathVariable Integer memberNum) {
+//		try {
+//			List<StoreDto> storeDtoList = mypageService.selectStoreList(memberNum);
+//			return new ResponseEntity<List<StoreDto>>(storeDtoList, HttpStatus.OK);
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//			return new ResponseEntity<List<StoreDto>>(HttpStatus.BAD_REQUEST);
+//		}
+//	}
+	
 //	@GetMapping("/selectStoreByStoreAddress") // Store.js
 //	@GetMapping("/selectStoreByName") // Store.js / CompainWrite.js
 //	@GetMapping("/selectMenuCategory") // MenuList.js
