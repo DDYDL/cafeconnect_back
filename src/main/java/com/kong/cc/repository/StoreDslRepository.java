07ff56ea6da5bbe7462458743f94f4a91e509c60
pporrorro_HJ <110891908+pporrorro@users.noreply.github.com>
2024-11-22 09:20:15 +0900

    add storeManageService
    StoreManageController 전반적 추가 및 수정

--- src/main/java/com/kong/cc/repository/StoreDslRepository.java
+++ src/main/java/com/kong/cc/repository/StoreDslRepository.java
@@ -16,6 +16,7 @@ public class StoreDslRepository {
 	@Autowired
 	private JPAQueryFactory jpaQueryFactory;
 	
+	// 가맹점 조회 페이지
 	public Long findStoreCount() throws Exception {
 		QStore store = QStore.store;
 		return jpaQueryFactory.select(store.count())
@@ -60,6 +61,122 @@ public class StoreDslRepository {
 		} else if(type.equals("storeAddress")) {
 			storeList = jpaQueryFactory.selectFrom(store)
 					.where(store.storeAddress.contains(word))
+					.orderBy(store.storeCode.desc())
+					.offset(pageRequest.getOffset())
+					.limit(pageRequest.getPageSize())
+					.fetch();
+		}
+		return storeList;
+	}
+	
+	// 삭제 요청된 가맹점 조회 (storeStatus = "Req")
+	public Long findDeleteReqStoreCount() throws Exception {
+		QStore store = QStore.store;
+		return jpaQueryFactory.select(store.count())
+				.from(store).where(store.storeStatus.contains("Req")).fetchOne();
+	}
+	
+	public List<Store> findDeleteReqStoreListByPaging(PageRequest pageRequest) throws Exception {
+		QStore store = QStore.store;
+		return jpaQueryFactory.selectFrom(store)
+				.where(store.storeStatus.contains("Req"))
+				.orderBy(store.storeCode.desc())
+				.offset(pageRequest.getOffset())
+				.limit(pageRequest.getPageSize())
+				.fetch();
+	}
+	
+	public Long searchDeleteReqStoreCount(String type, String word) throws Exception {
+		QStore store = QStore.store;
+		Long cnt = 0L;
+		if(type.equals("storeName")) {
+			cnt = jpaQueryFactory.select(store.count())
+					.from(store)
+					.where(store.storeName.contains(word))
+					.where(store.storeStatus.contains("Req"))
+					.fetchOne();
+		} else if(type.equals("storeAddress")) {
+			cnt= jpaQueryFactory.select(store.count())
+					.from(store)
+					.where(store.storeAddress.contains(word))
+					.where(store.storeStatus.contains("Req"))
+					.fetchOne();
+		}
+		return cnt;
+	}	
+	public List<Store> searchDeleteReqStoreListByPaging(PageRequest pageRequest, String type, String word) throws Exception {
+		QStore store = QStore.store;
+		List<Store> storeList = null;
+		if(type.equals("storeName")) {
+			storeList = jpaQueryFactory.selectFrom(store)
+					.where(store.storeName.contains(word))
+					.where(store.storeStatus.contains("Req"))
+					.orderBy(store.storeCode.desc())
+					.offset(pageRequest.getOffset())
+					.limit(pageRequest.getPageSize())
+					.fetch();
+		} else if(type.equals("storeAddress")) {
+			storeList = jpaQueryFactory.selectFrom(store)
+					.where(store.storeAddress.contains(word))
+					.where(store.storeStatus.contains("Req"))
+					.orderBy(store.storeCode.desc())
+					.offset(pageRequest.getOffset())
+					.limit(pageRequest.getPageSize())
+					.fetch();
+		}
+		return storeList;
+	}
+	
+	// 삭제된 가맹점 조회 (storeStatus = "Delete")
+	public Long findDeleteStoreCount() throws Exception {
+		QStore store = QStore.store;
+		return jpaQueryFactory.select(store.count())
+				.from(store).where(store.storeStatus.contains("Delete")).fetchOne();
+	}
+	
+	public List<Store> findDeleteStoreListByPaging(PageRequest pageRequest) throws Exception {
+		QStore store = QStore.store;
+		return jpaQueryFactory.selectFrom(store)
+				.where(store.storeStatus.contains("Delete"))
+				.orderBy(store.storeCode.desc())
+				.offset(pageRequest.getOffset())
+				.limit(pageRequest.getPageSize())
+				.fetch();
+	}
+	
+	public Long searchDeleteStoreCount(String type, String word) throws Exception {
+		QStore store = QStore.store;
+		Long cnt = 0L;
+		if(type.equals("storeName")) {
+			cnt = jpaQueryFactory.select(store.count())
+					.from(store)
+					.where(store.storeName.contains(word))
+					.where(store.storeStatus.contains("Delete"))
+					.fetchOne();
+		} else if(type.equals("storeAddress")) {
+			cnt= jpaQueryFactory.select(store.count())
+					.from(store)
+					.where(store.storeAddress.contains(word))
+					.where(store.storeStatus.contains("Delete"))
+					.fetchOne();
+		}
+		return cnt;
+	}	
+	public List<Store> searchDeleteStoreListByPaging(PageRequest pageRequest, String type, String word) throws Exception {
+		QStore store = QStore.store;
+		List<Store> storeList = null;
+		if(type.equals("storeName")) {
+			storeList = jpaQueryFactory.selectFrom(store)
+					.where(store.storeName.contains(word))
+					.where(store.storeStatus.contains("Delete"))
+					.orderBy(store.storeCode.desc())
+					.offset(pageRequest.getOffset())
+					.limit(pageRequest.getPageSize())
+					.fetch();
+		} else if(type.equals("storeAddress")) {
+			storeList = jpaQueryFactory.selectFrom(store)
+					.where(store.storeAddress.contains(word))
+					.where(store.storeStatus.contains("Delete"))
 					.orderBy(store.storeCode.desc())
 					.offset(pageRequest.getOffset())
 					.limit(pageRequest.getPageSize())
--- src/main/java/com/kong/cc/service/StoreManageService.java
+++ src/main/java/com/kong/cc/service/StoreManageService.java
@@ -6,6 +6,10 @@ import com.kong.cc.dto.StoreDto;
 import com.kong.cc.util.PageInfo;
 
 public interface StoreManageService {
-	List<StoreDto> storeList(PageInfo page, String type, String word) throws Exception;
+	List<StoreDto> storeList(PageInfo page, String type, String word, String status) throws Exception;
+	StoreDto storeDetail(Integer storeCode) throws Exception;
+	Integer createStoreCode() throws Exception; 
 	Integer addStore(StoreDto storeDto) throws Exception;
+	Integer deleteStore(Integer storeCode) throws Exception;
+	Integer restoreStore(Integer storeCode) throws Exception;
 }
