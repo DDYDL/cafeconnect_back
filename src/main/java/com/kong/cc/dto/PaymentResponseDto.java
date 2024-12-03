3460bcc6641a49084e742c6246d2f6d0d9876db8
ellie-park1992 <ellie.sy1992@gmail.com>
2024-12-03 18:28:20 +0900

    Update Order

--- src/main/java/com/kong/cc/dto/PaymentResponseDto.java
+++ src/main/java/com/kong/cc/dto/PaymentResponseDto.java
@@ -1,5 +1,7 @@
 package com.kong.cc.dto;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +10,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PaymentResponseDto {
-// 아임포트로 부터 응답 받을 데이터	
+	// 아임포트로 부터 응답 받을 데이터(결제 정보를 담음)
 	private String impUid; 
 	private String merchantUid; // 주문 번호
 	private String status; //결제 상태 실패,결제성공
 	private int amount; //결제 금액
+	
+	
+	private List<Integer> cartNums; //결제된 장바구니 
+	private Integer storeCode;
+	private String paymentMethod; 
 }
